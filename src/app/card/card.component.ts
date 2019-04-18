df0f61cb48bc8483444f1845bd4fff7e1b5954e0
Brandon Rabour <brandonrabour@yahoo.fr>
2019-04-19 01:35:12 +0200

    add buttons

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,5 +11,12 @@ export class CardComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+  bouton1() {document.getElementById("card7").style.borderColor = "gold";}
+  bouton2() {document.getElementById("card8").style.borderColor = "gold";}
+  bouton3() {document.getElementById("card9").style.borderColor = "gold";}
+  bouton4() {document.getElementById("card10").style.borderColor = "gold";}
+  bouton5() {document.getElementById("card11").style.borderColor = "gold";}
+  bouton6() {document.getElementById("card12").style.borderColor = "gold";}
 
 }
