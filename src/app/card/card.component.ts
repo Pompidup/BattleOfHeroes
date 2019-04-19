5eae978c2fff00f400fc0ae4c5d04fe152223e02
Pompidup <julienauvray28@gmail.com>
2019-04-19 05:32:34 +0200

    disparition cards

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { deck1 } from '../deck'
 import { deck2 } from '../deck'
 import { Card } from '../character'
+import { stringify } from '@angular/core/src/util';
 
 @Component({
   selector: 'app-card',
@@ -50,15 +51,22 @@ export class CardComponent implements OnInit {
     document.getElementById("card12").style.borderColor = "gold";
     document.getElementById("card2").style.borderColor = "red";
     this.fight(Number(deck1[5].skills[0]), Number(deck1[5].skills[1]), Number(deck1[5].skills[2]), Number(deck2[1].skills[0]), Number(deck2[1].skills[1]), Number(deck2[1].skills[2]));
+    this.hiddenCard("card12", "card2", "btn6");
   }
 
-  fight(d1s1:number, d1s2:number, d1s3:number, d2s1:number, d2s2:number, d2s3:number) {
+  fight(d1s1:number, d1s2:number, d1s3:number, d2s1:number, d2s2:number, d2s3:number):void {
     let scoreP1:number = 0;
     let scoreP2:number = 0;
     if (d1s1 > d2s1) {scoreP1++}else if (d1s1 < d2s1) {scoreP2++};
     if (d1s2 > d2s2) {scoreP1++}else if (d1s2 < d2s2) {scoreP2++};
     if (d1s3 > d2s3) {scoreP1++}else if (d1s3 < d2s3) {scoreP2++};
     if (scoreP1 > scoreP2){alert("1 point for player 1"); this.scorePlayer1++}else if (scoreP1 < scoreP2){alert("1 point for player 2"); this.scoreComputer++}else {alert("Match nul")};
+
+  }
+  hiddenCard(id1:string, id2:string, id3:string):void {
+    document.getElementById(id1).style.display = 'none';
+    document.getElementById(id2).style.display = 'none';
+    document.getElementById(id3).style.display = 'none';
 
   }
 
